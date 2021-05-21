b34b31574259f7439e9276728e260a133cf22484
ludov <ludov@LAPTOP-8O3J38D4>
2021-05-21 10:49:34 +0200

    non funziona neanche

--- src/it/polito/tdp/seriea/SerieAController.java
+++ src/it/polito/tdp/seriea/SerieAController.java
@@ -6,6 +6,8 @@ package it.polito.tdp.seriea;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import it.polito.tdp.seriea.model.Model;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ChoiceBox;
@@ -13,6 +15,7 @@ import javafx.scene.control.TextArea;
 
 public class SerieAController {
 
+	private Model model;
     @FXML // ResourceBundle that was given to the FXMLLoader
     private ResourceBundle resources;
 
@@ -41,7 +44,12 @@ public class SerieAController {
     @FXML // This method is called by the FXMLLoader when initialization is complete
     void initialize() {
         assert boxSeason != null : "fx:id=\"boxSeason\" was not injected: check your FXML file 'SerieA.fxml'.";
+       
         assert boxTeam != null : "fx:id=\"boxTeam\" was not injected: check your FXML file 'SerieA.fxml'.";
         assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'SerieA.fxml'.";
+    }
+    
+    public void setModel(Model m) {
+    	this.model = m;
     }
 }
